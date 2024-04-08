bd856b2320a27bad9025b8fd4a4e608cbeb4d9e6
Bodykudo <bodyalsadek2002@gmail.com>
2024-04-08 02:33:09 +0200

    Add Swagger documentation to backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //     transform: true,
-  //   }),
-  // );
+  const config = new DocumentBuilder()
+    .setTitle('Nexus Scan API')
+    .setDescription('Nexus Scan API Documentation')
+    .setVersion('0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(8000);
 }
 bootstrap();
